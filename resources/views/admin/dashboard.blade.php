f90960edd090f53e60033efc1ddaa9d18994125f
janpaul <janpaulalmanoche@gmail.com>
2020-08-26 18:16:14 +0800

    fix nav locations

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -4,7 +4,7 @@
 
 <!--breadcrumbs-->
 <div id="content-header">
-    <div id="breadcrumb"> <a href="index.html" title="Go to Home" class="tip-bottom"><i class="icon-home"></i> Home</a></div>
+    <div id="breadcrumb"> <a href="/admin/dashboard" title="Go to Home" class="tip-bottom"><i class="icon-home"></i> Home</a></div>
 </div>
 <!--End-breadcrumbs-->
 
--- resources/views/layouts/frontLayout/front_header.blade.php
+++ resources/views/layouts/frontLayout/front_header.blade.php
@@ -35,7 +35,7 @@ $mainCategories = Controller::mainCategories();
             <div class="row">
                 <div class="col-sm-4">
                     <div class="logo pull-left">
-                        <a href="index.html"><img src="{{asset('images/frontend_images/home/logo.png') }}" alt="" /></a>
+                        <a href="/"><img src="{{asset('images/frontend_images/home/logo.png') }}" alt="" /></a>
                     </div>
                     <div class="btn-group pull-right">
                         <div class="btn-group">
