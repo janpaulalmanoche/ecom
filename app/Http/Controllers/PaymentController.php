4da1d8b5e501aa018ea104c30bc10a55b4909b35
janpaul <janpaulalmanoche@gmail.com>
2020-08-26 17:55:24 +0800

    updated payment controller redirect url

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -17,7 +17,7 @@ class PaymentController extends Controller
         $this->client = new Client();
         $this->union_app_id = 'faa78f59-d055-4016-ab41-a3f812f78310';
         $this->union_app_secret = 'fQ3wA2kV1bH5lY2pS5iG7uT6vH0dS2jE8jG6kQ8nX1sE2pV0pH';
-        $this->redirect_url = "https://daae922f.ngrok.io/oauth2/callback";
+        $this->redirect_url = "http://ecom-reseller.herokuapp.com/oauth2/callback";
     }
 
     // public function make_sanbox_account()
